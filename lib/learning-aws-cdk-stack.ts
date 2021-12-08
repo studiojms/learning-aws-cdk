d82b8d47737fd1d909b9ffeac724f77bee517892
Jefferson Mariano de Souza <jefferson.msouza86@gmail.com>
2021-12-08 17:53:05 -0300

    integrating with geoglyphs

--- lib/learning-aws-cdk-stack.ts
+++ lib/learning-aws-cdk-stack.ts
@@ -1,7 +1,9 @@
 import { LambdaRestApi } from '@aws-cdk/aws-apigateway';
+import { Alarm } from '@aws-cdk/aws-cloudwatch';
 import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
 import * as cdk from '@aws-cdk/core';
 import { CfnOutput } from '@aws-cdk/core';
+import { SlackNotificationAction } from '@junglescout/geoglyphs';
 import * as path from 'path';
 
 export class LearningAwsCdkStack extends cdk.Stack {
@@ -14,6 +16,19 @@ export class LearningAwsCdkStack extends cdk.Stack {
       handler: 'index.handler',
       code: Code.fromAsset(path.resolve(__dirname, 'lambda')),
     });
+
+    const alarmAction = new SlackNotificationAction(this, {
+      webhookUrl: 'https://hooks.slack.com/services/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',
+    });
+
+    const alarm = new Alarm(this, 'alarm', {
+      metric: handler.metricErrors(),
+      threshold: 1,
+      datapointsToAlarm: 1,
+      evaluationPeriods: 1,
+    });
+
+    alarm.addAlarmAction(alarmAction);
 
     const gateway = new LambdaRestApi(this, 'gateway', {
       description: 'endpoint for simple lambda service',
